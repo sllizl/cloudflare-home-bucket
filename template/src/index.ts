9506eb180701d666acf52422b04973e9f0d8741f
last_whisper <sllizl917@outlook.com>
2026-02-16 08:05:54 +0900

    Update index.ts

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,4 +1,4 @@
-import { R2Explorer } from "r2-explorer";
+import { R2Explorer } from "../../packages/worker/dist/index.mjs";
 
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
 export default R2Explorer({
