26e6810c7bdac508c9684fa83c8d65a672d13908
last_whisper <sllizl917@outlook.com>
2026-02-16 08:22:20 +0900

    Update index.ts

--- template_1/src/index.ts
+++ template_1/src/index.ts
@@ -3,8 +3,8 @@ import { R2Explorer } from "../../packages/worker/dist/index.mjs";
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
 export default R2Explorer({
 	readonly: false,
-	// basicAuth: {
-	// 	username: "username",
-	// 	password: "password",
-	// },
+	 basicAuth: {
+	 	username: "user",
+	 	password: "123456",
+	 },
 });
