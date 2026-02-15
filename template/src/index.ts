a0325a20c8ef1e71b85c448232882075a8a19d94
last_whisper <sllizl917@outlook.com>
2026-02-16 08:22:56 +0900

    Update index.ts

--- template/src/index.ts
+++ template/src/index.ts
@@ -3,6 +3,7 @@ import { R2Explorer } from "../../packages/worker/dist/index.mjs";
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
 export default R2Explorer({
 	readonly: false,
+  cfAccessTeamName: 'sllizl',
 	// basicAuth: {
 	// 	username: "username",
 	// 	password: "password",
